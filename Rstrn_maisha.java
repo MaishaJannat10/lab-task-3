da4f5b6fbd4a860d4f569444a1a6b1aa629ed552
Maisha Jannat <maishajannatr10@gmail.com>
2025-02-17 21:43:42 +0600

    Add files via upload

---
+++ Rstrn_maisha.java
@@ -0,0 +1,24 @@
+
+package com.mycompany.mishas_project;
+
+
+public class Rstrn_maisha {public static void main(String[] args) {
+        
+        // Original string to be reversed
+        String ogStr = "CSE124";
+        
+        // Variable to hold the reversed string
+        String reversedStr = "";
+
+        // Loop through each character of the original string
+        for (int i = 0; i < ogStr.length(); i++) {
+            // Add the current character to the front of the reversed string
+            reversedStr = ogStr.charAt(i) + reversedStr;
+        }
+
+        // Print the reversed string
+        System.out.println("Reversed string: "+ reversedStr);
+    }
+}
+    
+
---
+++ Sum_its_jog_by_maisha.java
@@ -0,0 +1,32 @@
+
+package com.mycompany.mishas_project;
+
+import java.util.Scanner;
+
+
+public class Sum_its_jog_by_maisha {
+     public static void main(String[] args) {
+        // Prompt the user to enter two numbers
+        System.out.print("PLEASE ENTER TWO NUMBERS:");
+        
+        // Create a Scanner object to read input from the user
+        Scanner input = new Scanner(System.in);
+        
+        // Read the first number from the user
+        double num1 = input.nextDouble();
+        
+        // Read the second number from the user
+        double num2 = input.nextDouble();
+        
+        // Calculate the sum of the two numbers
+        double sum = num1 + num2;
+        
+        // Print the numbers entered by the user with two decimal places
+        System.out.printf("\n YOU HAVE ENTERED %.2f and %.2f", num1, num2);
+        
+        // Print the result of the sum
+        System.out.println("\n YOUR RESULT IS:" + sum);
+    }
+}
+    
+
